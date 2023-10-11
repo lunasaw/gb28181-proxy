b11ed956b78a90e33bf8ad875a5cfa9a3e5386e0
chenzhangyue <chenzhangyue01@weidian.com>
2023-10-11 15:13:30 +0800

    :tada: project init

---
+++ gb28181-client/src/main/java/io/github/lunasaw/Gb28181Client.java
@@ -0,0 +1,16 @@
+package io.github.lunasaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luna
+ * @date 2023/10/11
+ */
+@SpringBootApplication
+public class Gb28181Client {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gb28181Client.class, args);
+    }
+}
