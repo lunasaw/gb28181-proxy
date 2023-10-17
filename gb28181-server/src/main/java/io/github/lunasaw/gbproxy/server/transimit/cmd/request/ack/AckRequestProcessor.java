789915357dddaec241fa68a195544c830d31ba69
lunasaw <iszychen@gmail.com~>
2023-10-18 00:37:43 +0800

    type

---
+++ gb28181-server/src/main/java/io/github/lunasaw/gbproxy/server/transimit/cmd/request/ack/AckRequestProcessor.java
@@ -0,0 +1,35 @@
+package io.github.lunasaw.gbproxy.server.transimit.cmd.request.ack;
+
+import io.github.lunasaw.sip.common.transmit.event.request.SipRequestProcessorAbstract;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.sip.RequestEvent;
+
+/**
+ * SIP命令类型： 收到ACK请求
+ * 被叫方收到INVITE请求后，回复200OK，然后发起ACK请求
+ *
+ * @author weidian
+ */
+@Component
+@Getter
+@Setter
+public class AckRequestProcessor extends SipRequestProcessorAbstract {
+
+    public static final String METHOD = "ACK";
+
+    private String method = METHOD;
+
+    /**
+     * 收到ACK请求 处理
+     *
+     * @param evt
+     */
+    @Override
+    public void process(RequestEvent evt) {
+
+    }
+
+}
---
+++ gb28181-server/src/main/java/io/github/lunasaw/gbproxy/server/transimit/cmd/request/bye/ByeRequestProcessor.java
@@ -0,0 +1,34 @@
+package io.github.lunasaw.gbproxy.server.transimit.cmd.request.bye;
+
+import io.github.lunasaw.sip.common.transmit.event.request.SipRequestProcessorAbstract;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.sip.RequestEvent;
+
+/**
+ * SIP命令类型： 收到Bye请求
+ * 客户端发起Bye请求，结束通话
+ * @author weidian
+ */
+@Component
+@Getter
+@Setter
+public class ByeRequestProcessor extends SipRequestProcessorAbstract {
+
+    public static final String METHOD = "BYE";
+
+    private String method = METHOD;
+
+    /**
+     * 收到Bye请求 处理
+     *
+     * @param evt
+     */
+    @Override
+    public void process(RequestEvent evt) {
+
+    }
+
+}
