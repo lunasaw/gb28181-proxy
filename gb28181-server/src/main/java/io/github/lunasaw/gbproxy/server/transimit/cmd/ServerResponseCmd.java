742c5f65db860ba86c6fe172602f34fa4b36cff6
chenzhangyue <chenzhangyue01@weidian.com>
2023-10-19 11:18:26 +0800

    :page_facing_up: ServerResponseCmd

---
+++ gb28181-server/src/main/java/io/github/lunasaw/gbproxy/server/transimit/cmd/ServerResponseCmd.java
@@ -0,0 +1,40 @@
+package io.github.lunasaw.gbproxy.server.transimit.cmd;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.sip.header.Header;
+import javax.sip.message.Request;
+import javax.sip.message.Response;
+
+import io.github.lunasaw.sip.common.transmit.SipSender;
+import io.github.lunasaw.sip.common.utils.SipRequestUtils;
+
+/**
+ * @author luna
+ * @date 2023/10/19
+ */
+public class ServerResponseCmd {
+
+    public static void doResponseCmd(int statusCode, String phrase, String ip, Request request) {
+        doResponseCmd(statusCode, phrase, ip, request, new ArrayList<>());
+    }
+
+    public static void doResponseCmd(int statusCode, String phrase, String ip, Request request, Header... headers) {
+        doResponseCmd(statusCode, phrase, ip, request, Arrays.asList(headers));
+    }
+
+    public static void doResponseCmd(int statusCode, String phrase, String ip, Request request, List<Header> headers) {
+        try {
+            Response response = SipRequestUtils.createResponse(statusCode, request);
+            response.setReasonPhrase(phrase);
+            SipRequestUtils.setResponseHeader(response, headers);
+            SipSender.transmitRequest(ip, response);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- gb28181-server/src/main/java/io/github/lunasaw/gbproxy/server/transimit/request/register/RegisterRequestProcessor.java
+++ gb28181-server/src/main/java/io/github/lunasaw/gbproxy/server/transimit/request/register/RegisterRequestProcessor.java
@@ -1,18 +1,16 @@
 package io.github.lunasaw.gbproxy.server.transimit.request.register;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import javax.sip.RequestEvent;
-import javax.sip.header.AuthorizationHeader;
-import javax.sip.header.ContactHeader;
-import javax.sip.header.ViaHeader;
-import javax.sip.header.WWWAuthenticateHeader;
+import javax.sip.header.*;
 import javax.sip.message.Request;
 import javax.sip.message.Response;
 
-import io.github.lunasaw.sip.common.transmit.response.SipResponseProvider;
 import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.util.Lists;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
@@ -20,8 +18,8 @@ import com.luna.common.date.DateUtils;
 
 import gov.nist.javax.sip.header.SIPDateHeader;
 import gov.nist.javax.sip.message.SIPRequest;
+import io.github.lunasaw.gbproxy.server.transimit.cmd.ServerResponseCmd;
 import io.github.lunasaw.sip.common.entity.*;
-import io.github.lunasaw.sip.common.transmit.SipSender;
 import io.github.lunasaw.sip.common.transmit.event.request.SipRequestProcessorAbstract;
 import io.github.lunasaw.sip.common.utils.SipRequestUtils;
 import io.github.lunasaw.sip.common.utils.SipUtils;
@@ -65,6 +63,10 @@ public class RegisterRequestProcessor extends SipRequestProcessorAbstract {
             ToDevice toDevice = (ToDevice) registerProcessorServer.getToDevice(userId);
             FromDevice fromDevice = (FromDevice) registerProcessorServer.getFromDevice(sipUserId);
 
+            if (fromDevice == null || toDevice == null) {
+                return;
+            }
+
             RemoteAddressInfo remoteAddressInfo = SipUtils.getRemoteAddressFromRequest(request);
             String requestAddress = remoteAddressInfo.getIp() + ":" + remoteAddressInfo.getPort();
 
@@ -72,32 +74,36 @@ public class RegisterRequestProcessor extends SipRequestProcessorAbstract {
             log.info(title + "设备：{}, 开始处理: {}", userId, requestAddress);
 
             SipTransaction transaction = registerProcessorServer.getTransaction(userId);
+            // 设备接收到的IP地址，有可能是Nat之后的
+            String receiveIp = request.getLocalAddress().getHostAddress();
 
             RegisterInfo registerInfo = new RegisterInfo();
-
             registerInfo.setExpire(expires);
             registerInfo.setRegisterTime(DateUtils.getCurrentDate());
             // 判断TCP还是UDP
             ViaHeader reqViaHeader = (ViaHeader) request.getHeader(ViaHeader.NAME);
             String transport = reqViaHeader.getTransport();
             registerInfo.setTransport("TCP".equalsIgnoreCase(transport) ? "TCP" : "UDP");
-            registerInfo.setLocalIp(request.getLocalAddress().getHostAddress());
+            registerInfo.setLocalIp(receiveIp);
+            registerInfo.setRemotePort(remoteAddressInfo.getPort());
+            registerInfo.setRemoteIp(remoteAddressInfo.getIp());
+
             registerProcessorServer.updateRegisterInfo(userId, registerInfo);
 
             SipTransaction sipTransaction = SipUtils.getSipTransaction(request);
-            registerProcessorServer.updateSipTransaction(userId, sipTransaction);
-
             String callId = SipUtils.getCallId(request);
-            if (toDevice != null && transaction != null && callId.equals(transaction.getCallId())) {
+            List<Header> okHeaderList = getRegisterOkHeaderList(request);
+
+            if (transaction != null && callId.equals(transaction.getCallId())) {
                 log.info(title + "设备：{}, 注册续订: {}", userId, expires);
-                //
-                Response registerOkResponse = getRegisterOkResponse(request);
-                SipSender.transmitRequest(request.getLocalAddress().getHostAddress(), registerOkResponse);
+
+                ServerResponseCmd.doResponseCmd(Response.OK, "OK", receiveIp, request, okHeaderList);
+                registerProcessorServer.updateSipTransaction(userId, sipTransaction);
                 return;
             }
 
             String password = StringUtils.EMPTY;
-            if (fromDevice != null && StringUtils.isNotBlank(fromDevice.getPassword())) {
+            if (StringUtils.isNotBlank(fromDevice.getPassword())) {
                 password = fromDevice.getPassword();
             }
 
@@ -105,20 +111,14 @@ public class RegisterRequestProcessor extends SipRequestProcessorAbstract {
             if (authHead == null && StringUtils.isNotBlank(password)) {
 
                 // 认证密码不是空, 但是请求头中没有AuthorizationHeader
-
                 log.info(title + " 设备：{}, 回复401: {}", userId, requestAddress);
-
-
-                Response response = SipRequestUtils.createResponse(Response.UNAUTHORIZED, request);
 
                 String nonce = DigestServerAuthenticationHelper.generateNonce();
                 WWWAuthenticateHeader wwwAuthenticateHeader =
                         SipRequestUtils.createWWWAuthenticateHeader(DigestServerAuthenticationHelper.DEFAULT_SCHEME, fromDevice.getRealm(), nonce,
                                 DigestServerAuthenticationHelper.DEFAULT_ALGORITHM);
-                // okResponse.addHeader(wwwAuthenticateHeader);
-                response.addHeader(wwwAuthenticateHeader);
-                SipSender.transmitRequest(request.getLocalAddress().getHostAddress(), response);
 
+                ServerResponseCmd.doResponseCmd(Response.UNAUTHORIZED, "Unauthorized", receiveIp, request, wwwAuthenticateHeader);
                 return;
             }
 
@@ -128,42 +128,36 @@ public class RegisterRequestProcessor extends SipRequestProcessorAbstract {
 
             if (!passwordCorrect) {
                 // 注册失败
-                Response response = SipRequestUtils.createResponse(Response.FORBIDDEN, request);
-                response.setReasonPhrase("wrong password");
                 log.info(title + " 设备：{}, 密码/SIP服务器ID错误, 回复403: {}", userId, requestAddress);
-
-                SipSender.transmitRequest(request.getLocalAddress().getHostAddress(), response);
+                ServerResponseCmd.doResponseCmd(Response.FORBIDDEN, "wrong password", receiveIp, request);
                 return;
             }
 
             // 携带授权头并且密码正确
-            Response registerOkResponse = getRegisterOkResponse(request);
-            SipSender.transmitRequest(request.getLocalAddress().getHostAddress(), registerOkResponse);
+            ServerResponseCmd.doResponseCmd(Response.OK, "OK", receiveIp, request, okHeaderList);
             // 注册成功
+            registerProcessorServer.updateSipTransaction(userId, sipTransaction);
         } catch (Exception e) {
             log.error("未处理的异常 ", e);
         }
     }
 
-    private Response getRegisterOkResponse(Request request) {
-        // 携带授权头并且密码正确
-        Response response = SipRequestUtils.createResponse(Response.OK, request);
+    private List<Header> getRegisterOkHeaderList(Request request) {
+
+        List<Header> list = Lists.newArrayList();
         // 添加date头
         SIPDateHeader dateHeader = new SIPDateHeader();
         // 使用自己修改的
         GbSipDate gbSipDate = new GbSipDate(Calendar.getInstance(Locale.ENGLISH).getTimeInMillis());
         dateHeader.setDate(gbSipDate);
-        response.addHeader(dateHeader);
+        list.add(dateHeader);
 
         // 添加Contact头
-        response.addHeader(request.getHeader(ContactHeader.NAME));
+        list.add(request.getHeader(ContactHeader.NAME));
         // 添加Expires头
-        response.addHeader(request.getExpires());
-        return response;
+        list.add(request.getExpires());
+        return list;
 
     }
 
-    public void processRegister(RequestEvent evt) {
-        // 获取userId
-    }
 }
