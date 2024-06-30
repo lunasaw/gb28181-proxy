6752d4033102c20cb2c1dcbaa28a36330a45d09a
chenzhangyue <chenzhangyue01@weidian.com>
2024-06-30 09:57:46 +0800

    :wrench: feat: java 17

--- sip-common/src/main/java/io/github/lunasaw/sip/common/utils/DynamicTask.java
+++ sip-common/src/main/java/io/github/lunasaw/sip/common/utils/DynamicTask.java
@@ -7,8 +7,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.PostConstruct;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
